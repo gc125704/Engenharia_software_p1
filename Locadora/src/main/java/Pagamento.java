4e5c7709f906807e1fbe7b4cb156e65c8945ca43
gc125704 <45457121+gc125704@users.noreply.github.com>
2022-06-15 13:55:52 -0300

    Add files via upload

---
+++ Locadora/src/main/java/Pagamento.java
@@ -0,0 +1,6 @@
+/** @pdOid 35d6c4eb-8681-4087-86fc-ecb05a77ccf6 */
+public interface Pagamento {
+   /** @pdOid c01feed6-d3eb-429d-a980-c1475a0eefb9 */
+   void pagar();
+
+}
---
+++ Locadora/src/main/java/Titulo.java
@@ -0,0 +1,29 @@
+/** @pdOid 29bf4e06-ebe4-44da-adff-033a6df565a3 */
+public class Titulo {
+   /** @pdOid 3f555b10-fc35-4826-96ef-ba4a89535e37 */
+   private int descricao;
+   /** @pdOid da48c035-5fd3-458b-899d-699522b32735 */
+   private Midia midia;
+   
+   /** @pdOid 0487c368-7639-491c-991d-f911ef0b1884 */
+   protected int id;
+   
+   /** system.out.println("dfajdfl");
+    * 
+    * 
+    * @pdOid b07d48ea-20a7-4a9c-8c2a-7c68d8f54eb5 */
+   public void cadastrar() {
+   
+   }
+   
+   /** @pdOid bbaa3d97-4989-477f-8435-5977142092de */
+   public void alterar() {
+   
+   }
+   
+   /** @pdOid 49934d82-adf0-4a50-84aa-bd229239574d */
+   public void excluir() {
+   
+   }
+
+}
