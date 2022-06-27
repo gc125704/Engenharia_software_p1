d6d4aa12d23d7d0a5f9a92e1d8a5e4bd7cc1372b
gabriel <gabriel@DESKTOP-6EMC2SC>
2022-06-27 00:22:53 -0300

    .

--- Locadora/src/main/java/aplication/app.java
+++ Locadora/src/main/java/aplication/app.java
@@ -10,6 +10,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Scanner;
+import javax.xml.crypto.Data;
 
 /**
  *
@@ -17,14 +19,40 @@ import java.util.Date;
  */
 public class app {
 
+    public static void menu() {
+        System.out.println("+MENU+");
+        System.out.println("1-cadastrar cliente");
+        System.out.println("2-cadastrar midia");
+        System.out.println("3-reservar midia");
+        System.out.println("4-devolver midia");
+        System.out.println("5-excluir cliente");
+        System.out.println("6-excluir midia");
+        System.out.println("7-sair");
+        System.out.println("--------------------");
+        System.out.println("digite uma opcao");
+    }
+
+    public Date pegaData(String temp) throws ParseException {
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        Date data = formato.parse(temp);
+
+        return data;
+    }
+
     public static void main(String[] args) throws ParseException {
 
         try {
 
+            int opcao = 0;
+            int id = 0;
+            Date data;
+
+            Scanner scan = new Scanner(System.in);
+
             ArrayList<Cliente> clientes = new ArrayList<>();
 
             SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-            Date data = formato.parse("23/11/2015");
+            data = formato.parse("23/11/2015");
 
             Cliente cli = new Cliente(1, "Robson", "123456", data);
 
@@ -37,9 +65,27 @@ public class app {
             Titulo titulo = new Titulo("titulo1");
 
             Midia m = new Midia(0, titulo);
+            id++;
 
-            m.reservar(cli, m);
-            m.reservar(cli, m);
+            //m.reservar(clientes.get(0), m);
+            //m.reservar(cli, m);
+            while (opcao != 7) {
+                menu();
+                opcao = scan.nextInt();
+                switch (opcao) {
+
+                    case 1:
+                        System.out.println("digite o nome");
+                        String tempString = scan.nextLine();
+                        System.out.println("digite o CPF");
+                        String CPF = scan.nextLine();
+                        System.out.println("digite a data de nascimento");
+                        String temp = scan.nextLine();
+                        //data = pegaData(temp);
+                        break;
+
+                }
+            }
         } catch (Exception e) {
         }
     }
--- Locadora/src/main/java/estados/Disponivel.java
+++ Locadora/src/main/java/estados/Disponivel.java
@@ -8,33 +8,22 @@ import classes.Midia;
  */
 public class Disponivel implements Estado {
 
-    
-    public Disponivel(){
-        
-    }
-    /**
-     * @pdOid 9a0401fe-57c4-4cde-b9e1-84949c50a4ac
-     */
-    public void emprestar() {
-        
-    }
-
-    /**
-     * @pdOid 2528074f-8d09-4a91-898c-262aabc7ad07
-     */
-
-    /**
-     * @pdOid 5120ed2a-8d9d-4d44-a285-71c9b7f8aa51
-     */
-    public void devolver() {
-        System.out.println(" Mídia devolvida com sucesso ");
-    }
-
     @Override
     public void reservar(Cliente c, Midia m) {
         System.out.println("a midia " + m.getTituloTemp() + " foi reservada pelo cliente:");
         c.MostraCliente(c);
         m.setReserva();
+    }
+
+    /**
+     * @pdOid 9a0401fe-57c4-4cde-b9e1-84949c50a4ac
+     */
+    public void emprestar() {
+
+    }
+
+    public void devolver() {
+        System.out.println(" Mídia devolvida com sucesso ");
     }
 
 }
